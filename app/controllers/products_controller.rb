f1a39ed57e42ee8d114ddaa3afbc48ceb4344a14
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-06-07 23:31:54 +0200

    Init commit of example app with missing auth

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create product_params
+    redirect_to products_path
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+end
